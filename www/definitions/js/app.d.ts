94372094c0a0d10934ec4f222dacdcbc8ad4fd5b
Michael Knoch <info@michaelknoch.de>
2015-03-30 15:30:15 +0200

    remove www

--- www/definitions/js/app.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../typings/angularjs/angular.d.ts" />
-/// <reference path="../../typings/cordova-ionic/cordova-ionic.d.ts" />
-/// <reference path="../../typings/cordova/cordova.d.ts" />
