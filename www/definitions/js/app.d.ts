3d873a70c86da4911655d8ffb20c252dffacd073
Michael Knoch <info@michaelknoch.de>
2015-03-30 14:30:30 +0200

    stuff

---
+++ www/definitions/js/app.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/cordova-ionic/cordova-ionic.d.ts" />
+/// <reference path="../../typings/cordova/cordova.d.ts" />
