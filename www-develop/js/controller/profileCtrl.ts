60fbfbf3e61f74a8a5fcb3668b9284eddf859355
Michael Knoch <info@michaelknoch.de>
2015-03-31 15:53:14 +0200

    implement profile view

--- www-develop/js/controller/profileCtrl.ts
+++ www-develop/js/controller/profileCtrl.ts
@@ -1,16 +1,32 @@
 module Controller {
     export class ProfileCtrl {
-        result: string;
+        result:string;
+        _id:number;
+        picture:string;
+        name:string;
+        surname:string;
+        mail:string;
+        major:string;
+        semester:number;
+        subscribed_groups:number[];
 
         constructor(private $scope, private ProfileService) {
-            this.getData()
-        }
-        getData() {
-          this.ProfileService.getProfileData().then(profileData => {
-            this.result = profileData.name;
-          });
+            this.getData();
         }
 
-        static controllerId: string = "ProfileCtrl";
+        getData() {
+            this.ProfileService.getProfileData().then(profileData => {
+                this._id = profileData._id;
+                this.picture = profileData.picture;
+                this.name = profileData.name;
+                this.surname = profileData.surname;
+                this.mail = profileData.mail;
+                this.major = profileData.major;
+                this.semester = profileData.semester;
+                this.subscribed_groups = profileData.subscribed_groups;
+            });
+        }
+
+        static controllerId:string = "ProfileCtrl";
     }
 }
