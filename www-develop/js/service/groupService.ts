6db6c9bd45701b2237b277359fae22723cc706fc
Michael Knoch <info@michaelknoch.de>
2015-04-01 13:54:14 +0200

    add dummy mock

--- www-develop/js/service/groupService.ts
+++ www-develop/js/service/groupService.ts
@@ -6,8 +6,20 @@ module Service {
 
         myGroups() {
             return this.$q(function (resolve) {
-                _id:12333,
-                name:'Test';
+                [
+                    {
+                        "_id": "551bd8d98fe8ee54fe79fbe0",
+                        "name": "Digitaltechnik"
+                    },
+                    {
+                        "_id": "551bd8d96ac6271311173b03",
+                        "name": "Programmiertechnik"
+                    },
+                    {
+                        "_id": "551bd8d9c528474af52dbd7e",
+                        "name": "Mathe 1"
+                    }
+                ]
             });
         }
 
