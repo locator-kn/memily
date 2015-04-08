4c7caed7225ba1df0fb68a434706f8bb7dbdc90a
Michael Knoch <info@michaelknoch.de>
2015-04-08 14:38:10 +0200

    components

--- www-develop/js/controller/dashboardCtrl.ts
+++ www-develop/js/controller/dashboardCtrl.ts
@@ -2,7 +2,7 @@ module Controller {
     export class DashboardCtrl {
 
         constructor(private $scope, private DashboardService) {
-        
+
         }
 
         static controllerId:string = "DashboardCtrl";
