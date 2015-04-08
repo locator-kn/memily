11fd3860499f18dbe283ce71f61fee4c9d44a425
Michael Knoch <info@michaelknoch.de>
2015-04-08 14:39:38 +0200

    remove spinner module

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -23,7 +23,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-angular.module('starter', ['ionic', 'pascalprecht.translate', 'angularSpinner'])
+angular.module('starter', ['ionic', 'pascalprecht.translate'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
