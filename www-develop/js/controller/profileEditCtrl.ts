4e1cfe0434e2df3726793d92e09dc70ff1648832
Nicolai Löffler <nicolai.loeffler@web.de>
2015-04-09 12:21:18 +0200

    pic for profileedit, language

--- www-develop/js/controller/profileEditCtrl.ts
+++ www-develop/js/controller/profileEditCtrl.ts
@@ -1,10 +1,32 @@
 module Controller {
     export class ProfileEditCtrl {
 
+        result:string;
+        _id:number;
+        picture:string;
+        name:string;
+        surname:string;
         mail:string;
+        major:string;
+        semester:number;
+        subscribed_groups:number[];
 
-        constructor(private $scope, private $state) {
 
+        constructor(private $scope, private $state, private ProfileService) {
+            this.myProfile();
+        }
+
+        myProfile() {
+            this.ProfileService.myProfile().then(profileData => {
+                this._id = profileData._id;
+                this.picture = profileData.picture;
+                this.name = profileData.name;
+                this.surname = profileData.surname;
+                this.mail = profileData.mail;
+                this.major = profileData.major;
+                this.semester = profileData.semester;
+                this.subscribed_groups = profileData.subscribed_groups;
+            });
         }
 
         cancelEdit() {
